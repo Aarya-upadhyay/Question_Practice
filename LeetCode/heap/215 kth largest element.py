1690f9737540b6cf19bbd58c1205286144acf65c
Aarya Upadhyay <aaryau.2005@gmail.com>
2026-07-09 14:30:36 +0530

    Create 215 kth largest element.py

---
+++ LeetCode/heap/215 kth largest element.py
@@ -0,0 +1,18 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        """
+        sort karo desc me and find appropriate too (n log n)
+       
+        nums.sort(reverse=True)
+        return nums[k-1]
+         """
+        heap=[]
+        for i in range(k):
+            heapq.heappush(heap,nums[i])
+        for i in range(k,len(nums)):
+            if nums[i]> heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap,nums[i])
+        return heap[0]
+        
