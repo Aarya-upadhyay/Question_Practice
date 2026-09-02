af3072c65d9691d546f8819fdc141f3d1a88645c
Aarya Upadhyay <aaryau.2005@gmail.com>
2026-09-02 09:58:26 +0530

    Implement tabulation and space optimization for DP
    Added tabulation and space optimization methods for house robbers problem.

--- LeetCode/DP/house robbers.py
+++ LeetCode/DP/house robbers.py
@@ -14,3 +14,30 @@ def maxrob(arr,n,i,free,dp):
     return dp[i][free]
 r=maxrob(arr,n,0,1,dp)
 print(r)
+
+
+# tc - O(n) sc- O(n+2)= O(n)
+def tabulation(arr,n):
+    dp=[-1]*(n+2)
+    dp[n]=0
+    dp[n+1]=0
+    for i in range(n-1,-1,-1):
+        take=arr[i]+dp[i+2]
+        dont=dp[i+1]
+        dp[i]=max(take,dont)
+    return dp[0]
+print(tabulation(arr,n))
+
+#TC- O(n) SC= O(1)
+def spaceoptimization(arr,n):
+    nxt=0
+    n_nxt=0
+    max_curr=0
+    for i in range(n-1,-1,-1):
+        curr=arr[i]+n_nxt
+        do=nxt
+        max_curr=max(curr,do)
+        n_nxt=nxt
+        nxt=max_curr
+    return max_curr
+print(spaceoptimization(arr,n))
