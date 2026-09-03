bc5ddf5b24e931aba6360c9fb7b7e7fde96a8db5
Aarya Upadhyay <aaryau.2005@gmail.com>
2026-09-03 10:05:37 +0530

    Update unique paths II.py

--- LeetCode/DP/unique paths II.py
+++ LeetCode/DP/unique paths II.py
@@ -29,3 +29,44 @@ def dp(grid,i,j,t):
     t[i][j]=ans
     return ans
 print(dp(grid,0,0,t))
+
+def tabulation(grid,m,n):
+    dp=[[0]*n for _ in range(m)]
+    dp[m-1][n-1]=1
+    for i in range(m-1,-1,-1):
+        for j in range(n-1,-1,-1):
+            if i==m-1 and j==n-1:
+                continue
+            if grid[i][j]==1:
+                dp[i][j]=0
+                continue
+            d=0
+            r=0
+            if i+1<m:
+                d=dp[i+1][j]
+            if j+1<n:
+                r=dp[i][j+1]
+            dp[i][j]=d+r
+    return dp[0][0]
+n=len(grid[0])
+print(tabulation(grid,m,n))
+
+
+def uniquePathsWithObstacles(grid):
+
+    m = len(grid)
+    n = len(grid[0])
+
+    dp = [0] * n
+    dp[n-1] = 1
+
+    for i in range(m-1, -1, -1):
+        for j in range(n-1, -1, -1):
+
+            if grid[i][j] == 1:
+                dp[j] = 0
+            elif j + 1 < n:
+                dp[j] = dp[j] + dp[j+1]
+
+    return dp[0]
+print(uniquePathsWithObstacles(grid))
